ed1652a9ac878df4db3d4c0aa2decfae35dd27f1
Nazmul Hasan <Nazmul.Hasan@g.bracu.ac.bd>
2026-02-19 21:46:53 +0100

    2_deletion_of_linked_list.cpp

---
+++ 2_deletion_of_linked_list.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertattheHead(node *&head, int val)
+{
+    node *n = new node(val);
+    n->next = head;
+    head = n;
+}
+
+void insertAttheTail(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+
+    cout << "null" << endl;
+}
+
+void deleteatHead(node *&head)
+{
+    node *todelete = head;
+
+    head = head->next;
+
+    delete todelete;
+}
+
+void deletion(node *&head, int val)
+{
+
+    if (head == NULL)
+    {
+        return;
+    }
+
+    if (head->next == NULL)
+    {
+        deleteatHead(head);
+    }
+
+    node *temp = head;
+
+    while (temp->next->data != val)
+    {
+        temp = temp->next;
+    }
+    node *todelete = temp->next;
+    temp->next = temp->next->next;
+
+    delete temp->next;
+}
+
+int main()
+{
+    node *head = NULL;
+    insertAttheTail(head, 1);
+    insertAttheTail(head, 2);
+    insertAttheTail(head, 3);
+    // display(head);
+
+    // deletion(head, 3);
+    deleteatHead(head);
+    display(head);
+
+    return 0;
+}
