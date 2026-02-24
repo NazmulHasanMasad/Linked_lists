6d1fc3d72676b84c84600f2fb75547884d85b28b
Nazmul Hasan <Nazmul.Hasan@g.bracu.ac.bd>
2026-02-24 23:27:35 +0100

    21_Leetcode_86_partition_list.py

---
+++ 21_Leetcode_86_partition_list.py
@@ -0,0 +1,15 @@
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        left, right=ListNode(0),ListNode(0)
+        ltail,rtail=left,right
+        while head:
+            if head.val<x:
+                ltail.next=head
+                ltail=ltail.next
+            else:
+                rtail.next=head
+                rtail=rtail.next
+            head=head.next
+        ltail.next=right.next
+        rtail.next= None
+        return left.next
